b6d09406195084db444741179e98d86479628ed3
Cecilia <Cecilia@MacBook-Pro-de-Cecilia.local>
2014-11-13 13:02:00 +0100

    alerts

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,21 +3,54 @@ class EntriesController < ApplicationController
 		@project = Project.find params[:project_id]
 		@entries = @project.entries
 	end
-	# def show
-	# 	@project = Project.find params[:id]
-	# end
+
+	def show
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+	end
+
 	def new
 		@project = Project.find params[:project_id]
 		@entry = @project.entries.new
 	end
+
 	def create
 		@project = Project.find params[:project_id]
 		@entry = @project.entries.new entry_params
 
 		if @entry.save
+			flash[:notice] = "Entry created successfully"
 			redirect_to action: :index, controller: :entries, project_id: @project.id
 		else
+			flash[:alert] = "Something went wrong :("
 			render 'new'
+		end
+	end
+
+	def edit
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+	end
+
+	def update
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+
+		if @entry.update_attributes entry_params
+			redirect_to action: :show, controller: :entries, project_id: @project.id, id: @entry.id
+		else
+			render 'entry'
+		end
+	end
+
+	def destroy
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+
+		if @entry.destroy
+			redirect_to action: 'index'
+		else
+			redirect_to :back
 		end
 	end
 
@@ -26,4 +59,5 @@ class EntriesController < ApplicationController
 	def entry_params
 		params.require(:entry).permit(:hours, :minutes, :date)
 	end
+
 end
